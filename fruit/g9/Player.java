41c6fec7c3d78123c39a4079f736552bcbf3a909
Yuan Xu <yx2212@columbia.edu>
2013-10-22 18:47:58 -0400

    added g9, not implemented

---
+++ fruit/g9/Player.java
@@ -0,0 +1,18 @@
+package fruit.g9;
+
+import java.util.*;
+
+public class Player extends fruit.sim.Player
+{
+    public void init(int nplayers, int[] pref) {
+    }
+
+    public boolean pass(int[] bowl, int bowlId, int round,
+                        boolean canPick,
+                        boolean musTake) {
+        return random.nextDouble() > 0.5;
+    }
+    
+
+    private Random random = new Random();
+}
